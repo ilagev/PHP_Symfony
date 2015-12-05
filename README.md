5ca4c5bf46c278b89b13e76389690c4733288949
Ivan <ilagev@gmail.com>
2015-12-05 09:49:00 +0100

    Hasta listado de productos con links

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 miw_project
 ===========
 
-A Symfony project created on December 4, 2015, 4:45 pm.
+_Pasos_:
+
+1. Generar DemoBundle: *doctrine:generate:bundle* [namespace = Miw/DemoBundle, bundle name = nada, enter, yml, enter, enter... enter]
+    * genera src/Miw/DemoBundle con las carpetas controller (DefaultC.), depend., entity (aqui ira Producto.php), resources
+2. Generar Producto.php: *doctrine:generate:entity* [shortcut name = MiwDemoBundle:Producto, yml, insertar nombre + precio + descr., enter, enter]
+    * genera  Entity/Producto.php
+3. Crear rutas del producto:
+    3.1 añadir al app/config/routing.yml "miw_demo_producto"
+    3.2 crear DemoBundle/Resources/config/routing_producto.yml con miw_producto_nuevo + ...get
+    3.3 comprobar OK: **debug:router**
+4. Generar BBDD
+    4.1 *doctrine:schema:validate*
+    4.2 *doctrine:schema::update* (1o --dump, 2o --force)
+
--- src/Miw/DemoBundle/Controller/ProductoController.php
+++ src/Miw/DemoBundle/Controller/ProductoController.php
@@ -4,11 +4,30 @@ namespace Miw\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use \Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 use Miw\DemoBundle\Entity\Producto;
 
 class ProductoController extends Controller
 {
+    /*
+     * Recupera listado de productos
+     */
+    public function cgetAction() {
+        $productos = $this
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('MiwDemoBundle:Producto')
+                ->findAll();
+        
+        return $this->render('MiwDemoBundle:Producto:listado.html.twig', array(
+            'productos' => $productos
+        ));
+    }
+
     public function newAction()
     {
         $num = rand(0, 1000);
@@ -23,5 +42,30 @@ class ProductoController extends Controller
         $em->flush();
         
         return new Response('Id. Producto = ' . $producto->getId());
+    }
+    
+    public function getAction($id, $format) {
+        $em = $this->getDoctrine()->getManager();
+        
+        $producto = $em->getRepository('MiwDemoBundle:Producto')
+                       ->find(intval($id));
+        
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+        
+        if ($format === 'json') {
+            $jsonContent  = $serializer->serialize($producto, $format);
+            return new Response($jsonContent);
+        } else if ($format === 'xml') {
+            $xmlContent  = $serializer->serialize($producto, $format);
+            return new Response($xmlContent);
+        } else {
+            return $this->render('MiwDemoBundle:Producto:producto.html.twig', array(
+            'producto' => $producto
+        ));
+        }
+        
+        
     }
 }
